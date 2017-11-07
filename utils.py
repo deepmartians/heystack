43c8da511c0bc2523b8ec122c2eef34e3d520389
Disha19 <dishasinghania19@gmail.com>
2017-11-07 19:32:58 +0530

    Taking Input

---
+++ utils.py
@@ -0,0 +1,32 @@
+
+import os
+import argparse
+
+def parseArgs(_args):
+
+# python haystack.py analyze --input_dir /code/dir
+# python haystack.py search --keywords up down left
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input_dir", help="Path of the dir where audio files are present")
+	parser.add_argument("-k", "--keywords", help="The keywords to be searched for", nargs='+')
+
+	args = parser.parse_args()
+
+
+def parseInputDir(dir_path):
+	_mp3=[]
+	_wav=[]
+	for f in os.listdir(dir_path):
+		ff = os.path.join(dir_path,f)
+		if os.path.isfile(ff) :
+			if f.endswith(".mp3"):
+				_mp3.append(ff)
+			if f.endswith(".wav"):
+				_wav.append(ff)
+	inputdict = {'mp3' : _mp3, 'wav' : _wav}
+	print inputdict
+	return inputdict
+
+if __name__ == "__main__":
+	parseInputDir("/home/disha/code")
